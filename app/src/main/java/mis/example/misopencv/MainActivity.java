2038e5694fca9856f6bb2502c069598276f8fed7
Jawad Ahmed <jawadahmedtaj@gmail.com>
2019-05-07 17:54:47 +0200

    Completed

--- app/src/main/java/mis/example/misopencv/MainActivity.java
+++ app/src/main/java/mis/example/misopencv/MainActivity.java
@@ -7,25 +7,23 @@ import org.opencv.android.OpenCVLoader;
 import org.opencv.core.Mat;
 import org.opencv.android.CameraBridgeViewBase;
 import org.opencv.android.CameraBridgeViewBase.CvCameraViewListener2;
+import org.opencv.core.MatOfRect;
 import org.opencv.core.Point;
 import org.opencv.core.Rect;
 import org.opencv.core.Scalar;
+import org.opencv.core.Size;
 import org.opencv.imgproc.Imgproc;
-
+import org.opencv.objdetect.CascadeClassifier;
 import android.Manifest;
-import android.app.Activity;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
-import android.view.Menu;
 import android.view.MenuItem;
 import android.view.SurfaceView;
 import android.view.WindowManager;
-import android.widget.Toast;
-
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
@@ -38,20 +36,27 @@ public class MainActivity extends AppCompatActivity implements CvCameraViewListe
     private CameraBridgeViewBase    mOpenCvCameraView;
     private boolean                 mIsJavaCamera = true;
     private MenuItem                mItemSwitchCamera = null;
+    private CascadeClassifier faceCascade;
+    private CascadeClassifier noseCascade;
+    private Size faceClassifierSize, noseClassifierSize;
 
     private BaseLoaderCallback mLoaderCallback = new BaseLoaderCallback(this) {
         @Override
         public void onManagerConnected(int status) {
             switch (status) {
-                case LoaderCallbackInterface.SUCCESS:
-                {
+                case LoaderCallbackInterface.SUCCESS: {
                     Log.i(TAG, "OpenCV loaded successfully");
                     mOpenCvCameraView.enableView();
-                } break;
-                default:
-                {
+                    faceCascade = new CascadeClassifier();
+                    faceCascade.load(initAssetFile("haarcascade_frontalface_default.xml"));
+                    noseCascade = new CascadeClassifier();
+                    noseCascade.load(initAssetFile("haarcascade_mcs_nose.xml"));
+                }
+                break;
+                default: {
                     super.onManagerConnected(status);
-                } break;
+                }
+                break;
             }
         }
     };
@@ -60,18 +65,12 @@ public class MainActivity extends AppCompatActivity implements CvCameraViewListe
         Log.i(TAG, "Instantiated new " + this.getClass());
     }
 
-
-    /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         Log.i(TAG, "called onCreate");
         super.onCreate(savedInstanceState);
         getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
-
         setContentView(R.layout.activity_main);
-
-        // before opening the CameraBridge, we need the Camera Permission on newer Android versions
-
         if (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
             ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.CAMERA}, 0x123);
         } else {
@@ -82,20 +81,19 @@ public class MainActivity extends AppCompatActivity implements CvCameraViewListe
     }
 
     @Override
-    public void onPause()
-    {
+    public void onPause() {
         super.onPause();
         if (mOpenCvCameraView != null)
             mOpenCvCameraView.disableView();
     }
 
     @Override
-    public void onResume()
-    {
+    public void onResume() {
         super.onResume();
+
         if (!OpenCVLoader.initDebug()) {
             Log.d(TAG, "Internal OpenCV library not found. Using OpenCV Manager for initialization");
-            OpenCVLoader.initAsync(OpenCVLoader.OPENCV_VERSION_3_0_0, this, mLoaderCallback);
+            OpenCVLoader.initAsync(OpenCVLoader.OPENCV_VERSION_3_4_0, this, mLoaderCallback);
         } else {
             Log.d(TAG, "OpenCV library found inside package. Using it!");
             mLoaderCallback.onManagerConnected(LoaderCallbackInterface.SUCCESS);
@@ -109,31 +107,30 @@ public class MainActivity extends AppCompatActivity implements CvCameraViewListe
     }
 
     public void onCameraViewStarted(int width, int height) {
+        faceClassifierSize = new Size(50, 50);
+        noseClassifierSize = new Size(2, 2);
     }
 
     public void onCameraViewStopped() {
     }
 
     public Mat onCameraFrame(CvCameraViewFrame inputFrame) {
-
-        //return inputFrame.rgba();
-        /*
-        Mat col  = inputFrame.rgba();
-        Rect foo = new Rect(new Point(100,100), new Point(200,200));
-        Imgproc.rectangle(col, foo.tl(), foo.br(), new Scalar(0, 0, 255), 3);
-        return col;
-        */
-
+        MatOfRect faces = new MatOfRect();
         Mat gray = inputFrame.gray();
-        Mat col  = inputFrame.rgba();
-
-        Mat tmp = gray.clone();
-        Imgproc.Canny(gray, tmp, 80, 100);
-        Imgproc.cvtColor(tmp, col, Imgproc.COLOR_GRAY2RGBA, 4);
-
+        Mat col = inputFrame.rgba();
+        if (faceCascade != null) {
+            faceCascade.detectMultiScale(gray, faces, 1.2, 0, 0, faceClassifierSize, new Size());
+        }
+        for (Rect face: faces.toArray()) {
+            MatOfRect noses = new MatOfRect();
+            Mat faceArea = gray.submat(new Rect(face.x, face.y, face.width, face.height));
+            noseCascade.detectMultiScale(faceArea, noses, 1.2, 0, 0, noseClassifierSize, new Size());
+            for (Rect nose: noses.toArray()) {
+                Imgproc.circle(col, new Point(nose.x + nose.width / 2, nose.y + nose.height / 2),nose.width * 2, new Scalar(255, 0, 0),10);
+            }
+        }
         return col;
     }
-
 
     public String initAssetFile(String filename)  {
         File file = new File(getFilesDir(), filename);
